a17f841626ce2bb400e12798ab8a54f020469f89
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2024-02-22 17:20:43 +0100

    prep: master plan pipeline

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os.path
+import json
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -25,6 +26,22 @@ def main():
     document = service.documents().get(documentId=DOCUMENT_ID).execute()
 
     print(f"The title of the document is: {document.get('title')}")
+    print(json.dumps(document.get('body'), indent="  "))
+    ###### master plan pipeline:
+    # >put JSON in a .json
+    # >make stash of functions to handle different parts of it
+    # >first, the hard part - search for all images and their positions
+    # >download all of the images to ../yoinkstash/pos_objs/rawposobjs.json
+    # >then grab all the text and tables
+    # > >put tables in a ../yoinkstash/tables/tables.json
+    # > >rest goes to ../yoinkstash/text/rawtext.json
+    # > >develop some system to identify your headers and etc
+    # > > >different for 0, different for other notebookies.
+    # > > >split rawtext.json into segtext.json (segmented text)
+    # ! everything stores its position!
+    # >the first hellish attempt at trying to convert everything to XML or even HTML...
+    # > >....but maybe converting the JSON data into a very decent XML format i can engineer is a better idea?
+
   except HttpError as err:
     print(err)
 
